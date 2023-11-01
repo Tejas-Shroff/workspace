138b7746cb1b3813e6667205fba80dddb7c7e9e8
Iamneo <techadmin@iamneo.ai>
2023-11-01 16:00:36 +0000

    Backup Commit

--- dotnetapp/Controllers/DealerController.cs
+++ dotnetapp/Controllers/DealerController.cs
@@ -1,6 +1,10 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using dotnetapp.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.EntityFrameworkCore.Design;
+
 
 namespace dotnetapp.Controllers;
 
@@ -14,9 +18,32 @@ public class DealerControllers : Controller {
     public IActionResult List(){
         return View();
     }
-    public IActionResult Create()[
-         
-    ]
+    public IActionResult Create(){
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Create(Dealer d){
+        if(ModelState.IsValid){
+            context.Dealer.Add(d);
+            context.SaveChanges();
+            return RedirectToAction("List");
+
+        }
+        return View();
+    }
+    public IActionResult Delete(int ID){
+        var data = context.Dealer.Find(id);
+        return View(data);
+    }
+    [HttpPost]
+    public IActionResult Delete(Dealer d){
+
+        Dealer dl = context.Dealer.Find(d.ID);
+        context.Dealer.Remove(d);
+        context.Dealer.SaveChanges();
+        return RedirectToAction()
+      
+    }
 
 }
 
