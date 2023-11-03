7477738fd878e53d2e0b73a306ed4034926a9581
Iamneo <techadmin@iamneo.ai>
2023-11-03 06:50:29 +0000

    Backup Commit

--- demowebapi/Controllers/FruitsController.cs
+++ demowebapi/Controllers/FruitsController.cs
@@ -6,6 +6,10 @@ namespace demowebapi.Controllers;
 [ApiController]
 [Route("api/[Controller]")]
 public class FruitsController : ControllerBase{
-    static List<string> Fruits = new List<string>
+    static List<string> Fruits = new List<string> { "Apple" , "Mango", "Orange", "Grapes", "Banana" };
+    [HttpGet]
+    public IEnumerable<string>Get(){
+        return Fruits;
+    }
 
 }
