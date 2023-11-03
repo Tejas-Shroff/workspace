37f4b8f0c3b3a300df9569e3c7b8c0e875195b6e
Iamneo <techadmin@iamneo.ai>
2023-11-03 08:50:54 +0000

    Backup Commit

--- demowebapi/Controllers/FruitsController.cs
+++ demowebapi/Controllers/FruitsController.cs
@@ -1,22 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-namespace demowebapi.Controllers;
+namespace demowebapi.Controllers{
+
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class FruitsController : ControllerBase{
+        static List<string> Fruits = new List<string> { "Apple" , "Mango", "Orange", "Grapes", "Banana" };
+        [HttpGet]
+        [Route("showFruits")]
+        public IEnumerable<string>GetFruits(){
+            return Fruits;
+        }
+        [HttpGet]
+        [Route("ShowFruits/{id}")]
 
 
-[ApiController]
-[Route("api/[Controller]")]
-public class FruitsController : ControllerBase{
-    static List<string> Fruits = new List<string> { "Apple" , "Mango", "Orange", "Grapes", "Banana" };
-    [HttpGet]
-    [Route("showFruits")]
-    public IEnumerable<string>GetFruits(){
-        return Fruits;
+        public string GetFruits(int id){
+            return Fruits[id];
+        }
+        [HttpPost]
+        [Route("Add Fruits")]
+        public void Post([FromBody] string data){
+
+            Fruits.Add(data);
+
+        }
     }
-
-
-    public string GetFruits(int id){
-        return Fruits[id];
-    }
-    public void P
-
 }
+
+
