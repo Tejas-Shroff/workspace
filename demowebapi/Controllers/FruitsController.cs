c300651c3f4041cdd9448d594ce4f7e091439fb2
Iamneo <techadmin@iamneo.ai>
2023-11-03 06:35:29 +0000

    Backup Commit

---
+++ demowebapi/Controllers/FruitsController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+namespace demowebapi.Controllers;
+
+
+[ApiController]
+[Route("api/[Controller]")]
+public class FruitsController : ControllerBase{
+    static List<string> Fruits = new List<string>
+
+}
