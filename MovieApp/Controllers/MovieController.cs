e2c28e9af772300822030916c9634d33dbedc1e4
Iamneo <techadmin@iamneo.ai>
2023-11-04 07:04:08 +0000

    Backup Commit

--- MovieApp/Controllers/MovieController.cs
+++ MovieApp/Controllers/MovieController.cs
@@ -2,8 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 namespace MovieApp.Controllers{
 
     [ApiController]
-    [Route("/controller")]
-    public class MoviewController : controllerBase{
+    [Route("/[controller]")]
+    public class MovieController : ControllerBase{
         MovieContext context = new MovieContext();
         [HttpGet]
         [Route("ListMovies")]
@@ -14,6 +14,20 @@ namespace MovieApp.Controllers{
 
 
         }
-        
+        [HttpGet]
+        [Route("ListMovies/{id}")]
+        public IActionResult Get(int id){
+            if(id == null){
+                return BadRequest("Id cannot be null");
+
+            }
+            var data = (from m in context.Movies where m.Id == id select m ).FirstOrDefault();
+            if(data == null){
+                return NotFound($"Movie {id} not found");
+
+            }
+            return Ok(data);
+            
+        }
     }
 }
