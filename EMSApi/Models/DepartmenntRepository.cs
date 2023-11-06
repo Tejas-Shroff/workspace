2c96191aa9e80d97f9d4cc4c6979ed3c127d4750
Iamneo <techadmin@iamneo.ai>
2023-11-06 09:53:42 +0000

    Backup Commit

--- EMSApi/Models/DepartmenntRepository.cs
+++ EMSApi/Models/DepartmenntRepository.cs
@@ -4,5 +4,21 @@ namespace EMSApi.Models;
 
 public class DepartmentRepository : IDept{
     EmsDbContext context = new EmsDbContext();
-    public void 
+    public void AdDept(Department dept){
+        context.DepartmentRepository.Add(dept);
+        context.SaveChanges();
+
+    }
+    public void DeleteDept(int id){
+
+    }
+    public void EditDept(Department dept){
+
+    }
+    public Department FindDept(int id){
+
+    }
+    public List<Department> GetDepartments(){
+        
+    }
 }
