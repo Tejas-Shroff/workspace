4b72650e46945e165fac525952cb4a5d5b5525c0
Iamneo <techadmin@iamneo.ai>
2023-11-15 11:32:10 +0000

    Backup Commit

--- AngMovie/src/app/app-routing.module.ts
+++ AngMovie/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListMovieComponent } from './list-movie/list-movie.component';
+import { FindmovieComponent } from './findmovie/findmovie.component';
 
 
-const routes: Routes = [];
-{path: ListMovies,component}
+const routes: Routes = [
+
+  {path: 'ListMovies',component:ListMovieComponent},
+  {path:'find',component:FindmovieComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
