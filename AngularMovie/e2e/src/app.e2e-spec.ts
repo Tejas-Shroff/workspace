d4804e68c63a29bbb384ecbdd928c7015734d68a
Iamneo <techadmin@iamneo.ai>
2023-11-15 07:32:11 +0000

    Backup Commit

--- AngularMovie/e2e/src/app.e2e-spec.ts
+++ AngularMovie/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('MovieAngular app is running!');
+    expect(page.getTitleText()).toEqual('AngularMovie app is running!');
   });
 
   afterEach(async () => {
--- MovieApp/Program.cs
+++ MovieApp/Program.cs
@@ -30,7 +30,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+`
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
